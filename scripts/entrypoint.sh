d831e43d144ce0fba84eda261bd8359766ce6e06
jbgithub22 <jb.github22@gmail.com>
2024-08-19 13:28:49 +0800

    Updated project structure

---
+++ scripts/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Start Spark master
+$SPARK_HOME/sbin/start-master.sh
+
+# Start Spark worker
+$SPARK_HOME/sbin/start-worker.sh spark://$(hostname):7077
+
+# Keep the container running
+tail -f /dev/null
